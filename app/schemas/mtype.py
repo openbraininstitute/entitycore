ee7359376f77e00df9ec1a3f7ffb71ceb65b798d
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-02-05 14:32:27 +0100

    add `/mtype` endpoint

---
+++ app/schemas/mtype.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, ConfigDict
+
+from app.schemas.base import CreationMixin
+
+
+class MTypeBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    pref_label: str
+    alt_label: str
+    definition: str
+
+
+class MTypeRead(MTypeBase, CreationMixin):
+    pass

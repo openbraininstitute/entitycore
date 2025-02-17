9c41ffe1d975cee99fb71836719e06b47651ded6
MikeG <mgevaert@openbraininstitute.org>
2025-02-17 11:01:17 +0100

    Update facet output (#39)
    * update facet shape, switch output to new format
    * Simplify queries, move to to sqlalchemy v2 style, by Gianluca
    
    ---------
    
    Co-authored-by: Gianluca Ficarelli <gianluca.ficarelli@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -46,17 +46,24 @@ def skip_project_check():
 
 
 def create_reconstruction_morphology_id(
-    client, species_id, strain_id, brain_region_id, headers, authorized_public
+    client,
+    species_id,
+    strain_id,
+    brain_region_id,
+    headers,
+    authorized_public,
+    name="Test Morphology Name",
+    description="Test Morphology Description",
 ):
     response = client.post(
         "/reconstruction_morphology/",
         headers=headers,
         json={
+            "name": name,
+            "description": description,
             "brain_region_id": brain_region_id,
             "species_id": species_id,
             "strain_id": strain_id,
-            "description": "Test Morphology Description",
-            "name": "Test Morphology Name",
             "brain_location": {"x": 10, "y": 20, "z": 30},
             "legacy_id": "Test Legacy ID",
             "authorized_public": authorized_public,
@@ -64,3 +71,10 @@ def create_reconstruction_morphology_id(
     )
     assert response.status_code == 200
     return response.json()["id"]
+
+
+def add_db(db, row):
+    db.add(row)
+    db.commit()
+    db.refresh(row)
+    return row

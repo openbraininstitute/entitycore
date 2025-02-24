15da590b23a368cf79c5eb2c095f11631e09d3ca
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-02-24 16:10:40 +0100

    switch app/routers/species.py

--- app/routers/species.py
+++ app/routers/species.py
@@ -1,8 +1,11 @@
+import sqlalchemy as sa
 from fastapi import APIRouter
 
 from app.db.model import Species
+from app.dependencies import PaginationQuery
 from app.dependencies.db import SessionDep
 from app.errors import ensure_result
+from app.routers.types import ListResponse, PaginationResponse
 from app.schemas.base import (
     SpeciesCreate,
 )
@@ -16,9 +19,29 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=list[SpeciesRead])
-def read_role(db: SessionDep, skip: int = 0, limit: int = 10):
-    return db.query(Species).offset(skip).limit(limit).all()
+@router.get("/", response_model=ListResponse[SpeciesRead])
+def read_role(db: SessionDep, pagination_request: PaginationQuery):
+    query = sa.select(Species)
+
+    data = db.execute(
+        query.offset(pagination_request.page * pagination_request.page_size).limit(
+            pagination_request.page_size
+        )
+    ).scalars()
+
+    total_items = db.execute(query.with_only_columns(sa.func.count())).scalar_one()
+
+    response = ListResponse[SpeciesRead](
+        data=data,
+        pagination=PaginationResponse(
+            page=pagination_request.page,
+            page_size=pagination_request.page_size,
+            total_items=total_items,
+        ),
+        facets=None,
+    )
+
+    return response
 
 
 @router.get("/{role_id}", response_model=SpeciesRead)
--- tests/test_species.py
+++ tests/test_species.py
@@ -1,7 +1,34 @@
+ROUTE = "/species/"
+
+
 def test_create_species(client):
-    response = client.post("/species/", json={"name": "Test Species", "taxonomy_id": "12345"})
+    name = "Test Species"
+    response = client.post(ROUTE, json={"name": name, "taxonomy_id": "12345"})
     assert response.status_code == 200
     data = response.json()
     assert data["name"] == "Test Species"
     assert data["taxonomy_id"] == "12345"
     assert "id" in data
+    id_ = data["id"]
+
+    response = client.get(f"{ROUTE}{id_}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == id_
+    assert data["name"] == name
+    assert data["taxonomy_id"] == "12345"
+
+    response = client.get(ROUTE)
+    assert response.status_code == 200
+    data = response.json()["data"]
+    assert len(data) == 1
+    assert data[0]["name"] == name
+    assert data[0]["id"] == id_
+
+
+def test_missing_role(client):
+    response = client.get(f"{ROUTE}42424242")
+    assert response.status_code == 404
+
+    response = client.get(f"{ROUTE}notanumber")
+    assert response.status_code == 422

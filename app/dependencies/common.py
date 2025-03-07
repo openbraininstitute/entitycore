1a54d7515e1535f0319f48f9493fe795e9cdd9ff
Gianluca Ficarelli <gianluca.ficarelli@gmail.com>
2025-03-07 08:05:08 +0100

    Hide all the params not included in the schema

--- app/dependencies/common.py
+++ app/dependencies/common.py
@@ -20,9 +20,15 @@ def forbid_extra_query_params(
     but this option is intended for internal use, and it might be removed in the future.
     """
 
-    def traverse(dependant: Dependant, params: set) -> set:
-        """Update params from the query_params of all the dependencies."""
-        params.update(param.alias for param in dependant.query_params)
+    def traverse(dependant: Dependant, params: dict[str, bool]) -> dict[str, bool]:
+        """Update params from the query_params of all the dependencies.
+
+        Return a dict of all the known params and the boolean values of `include_in_schema`.
+        """
+        params.update(
+            (param.alias, getattr(param.field_info, "include_in_schema", True))
+            for param in dependant.query_params
+        )
         for dependency in dependant.dependencies:
             traverse(dependency, params)
         return params
@@ -30,17 +36,21 @@ def forbid_extra_query_params(
     if allow_extra_params:
         return
 
-    # allowed_params could be cached by (request.scope["method"], request.scope["path"])
-    allowed_params = traverse(request.scope["route"].dependant, params=set())
+    # possible improvement: cache allowed_params by request.scope["method"], request.scope["path"]
+    allowed_params = traverse(request.scope["route"].dependant, params={})
     query_params = set(request.query_params.keys())
-    if unknown_params := query_params - allowed_params:
+    if unknown_params := query_params.difference(allowed_params):
         raise ApiError(
             message="Unknown query parameters",
             error_code=ApiErrorCode.INVALID_REQUEST,
             http_status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
             details={
                 "unknown_params": sorted(unknown_params),
-                "allowed_params": sorted(allowed_params - {"allow_extra_params"}),
+                "allowed_params": sorted(
+                    param
+                    for param, include_in_schema in allowed_params.items()
+                    if include_in_schema
+                ),
             },
         )
 

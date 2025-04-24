463411786b1c7086f82d00c8aaf25a48863001ab
Gil Barrios <gbarrios@MacBook-Pro-de-Gil.local>
2025-04-24 10:32:37 +0200

    remove point_process

--- app/schemas/ion_channel_model.py
+++ app/schemas/ion_channel_model.py
@@ -17,7 +17,6 @@ class NmodlParameters(BaseModel):
     range: list[str]
     read: list[str] | None = None
     suffix: str | None = None
-    point_process: str | None = None
     useion: list[str] | None = None
     write: list[str] | None = None
     nonspecific: list[str] | None = None

b6c2b78d469c6e53cdec532407d91fc6dfcb4b40
ilkankilic <ilkan.kilic@openbraininstitute.org>
2025-04-30 10:54:20 +0200

    fix typo

--- app/cli/utils.py
+++ app/cli/utils.py
@@ -455,7 +455,7 @@ def import_ion_channel_model(script: dict[str, Any], project_context: ProjectCon
         legacy_id=[legacy_id],
         legacy_self=[legacy_self],
         name=script["name"],
-        nmodel_suffix=script.get("suffix"),
+        nmodl_suffix=script.get("suffix"),
         description=script.get("description", ""),
         is_ljp_corrected=script.get("isLjpCorrected", False),
         is_temperature_dependent=script.get("isTemperatureDependent", False),

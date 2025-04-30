36525da93a4698f2d8147a0643189d24c28fe597
dkeller9 <daniel.keller@openbraininstitute.org>
2025-04-30 08:22:12 +0200

    the ScientificArtifact.py file was added, but not mixed in
    This branch is just intended for evaluation by the scientific team.

---
+++ app/schemas/scientific_artifact.py
@@ -0,0 +1,38 @@
+import uuid
+from datetime import datetime, timedelta
+from typing import Annotated, TYPE_CHECKING
+from pydantic import UUID4, BaseModel, ConfigDict
+
+from datetime import date
+
+#if TYPE_CHECKING:
+from app.schemas.agent import PersonRead
+from app.schemas.contribution import ContributionReadWithoutEntity
+ 
+# where was the artifact published?
+class IsDefinedByType(BaseModel):
+    """
+    Represents ways to define or identify an entity, such as by DOI, PMID, UUID, or other means.
+    All fields are optional.
+    """
+    DOI: str | None = None  # Optional DOI (Digital Object Identifier) as a string
+    PMID: int | None = None # Optional PMID (PubMed Identifier) as an integer
+    UUID: uuid.UUID | None = None # Using uuid.UUID type for clarity, assuming it refers to a standard UUID
+    Other: str | None = None # Optional alternative identifier as a string
+
+class ScientificArtifactMixin(BaseModel):
+    name :str
+    description:str  
+    subject_id : uuid.UUID
+    
+    brain_region_id: int
+    contributions: list["ContributionReadWithoutEntity"] | None
+    license_id: uuid.UUID 
+    experiment_date: date 
+
+    IsDefinedBy : IsDefinedByType
+    
+    validation_tags: dict[str, bool] #This is a dict{“properties_check”: T/F} (determined by a script not a user input. Should this be here or an annotation?) 
+
+    contact_id : uuid.UUID
+    contact_email: str

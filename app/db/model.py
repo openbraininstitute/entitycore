ed9331e7c953b04100f50c27fbc4047190247abb
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-05-02 12:16:14 +0200

    currents are floats

--- app/db/model.py
+++ app/db/model.py
@@ -464,8 +464,8 @@ class MEModel(
         "ReconstructionMorphology", foreign_keys=[morphology_id], uselist=False
     )
 
-    holding_current: Mapped[int]
-    threshold_current: Mapped[int]
+    holding_current: Mapped[float]
+    threshold_current: Mapped[float]
 
     emodel_id: Mapped[uuid.UUID] = mapped_column(ForeignKey(f"{EntityType.emodel}.id"))
 

4d30530784c3ccac4923f05641d5337236a15c82
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-05-20 14:16:47 +0200

    Added very basic "Read One" endpoint for circuits

--- app/schemas/circuit.py
+++ app/schemas/circuit.py
@@ -5,16 +5,19 @@ from pydantic import BaseModel, ConfigDict
 
 from app.db.model import CircuitBuildCategory, CircuitScale
 from app.schemas.base import (
-    AuthorizationMixin,
+    # AuthorizationMixin,
     AuthorizationOptionalPublicMixin,
-    BrainRegionRead,
-    CreationMixin,
-    EntityTypeMixin,
-    IdentifiableMixin,
-    SpeciesRead,
-    StrainRead,
+    LicensedCreateMixin,
+    # BrainRegionReadMixin,
+    # CreationMixin,
+    # EntityTypeMixin,
+    # IdentifiableMixin,
+    # SpeciesRead,
+    # StrainRead,
 )
-from app.schemas.contribution import ContributionReadWithoutEntity
+# from app.schemas.contribution import ContributionReadWithoutEntityMixin
+# from app.schemas.asset import AssetsMixin
+# from app.schemas.subject import SubjectRead
 
 
 class CircuitBase(BaseModel):
@@ -24,35 +27,36 @@ class CircuitBase(BaseModel):
 
 
 class CircuitCreate(CircuitBase, AuthorizationOptionalPublicMixin, LicensedCreateMixin):
-    subject_id: uuid.UUID
-    species_id: uuid.UUID
-    brain_region_id: uuid.UUID
-    experiment_date: datetime | None
-    published_in : dict | None
-    contact_id : uuid.UUID | None
+    pass
+    # subject_id: uuid.UUID
+    # species_id: uuid.UUID
+    # brain_region_id: uuid.UUID
+    # experiment_date: datetime | None
+    # published_in : dict | None
+    # contact_id : uuid.UUID | None
 
-    parent_circuit_id: uuid.UUID | None
-    atlas_id: uuid.UUID | None
-    build_category: CircuitBuildCategory
-    scale: CircuitScale
-    has_morphologies: bool
-    has_point_neurons: bool
-    has_electrical_cell_models: bool
-    has_spines: bool
-    is_simulatable: bool
-    version: str | None
+    # parent_circuit_id: uuid.UUID | None
+    # atlas_id: uuid.UUID | None
+    # build_category: CircuitBuildCategory
+    # scale: CircuitScale
+    # has_morphologies: bool
+    # has_point_neurons: bool
+    # has_electrical_cell_models: bool
+    # has_spines: bool
+    # is_simulatable: bool
+    # version: str | None
 
 
 class CircuitRead(
     CircuitBase,
-    BrainRegionReadMixin,
-    AuthorizationMixin,
-    IdentifiableMixin,
-    CreationMixin,
-    EntityTypeMixin,
-    AssetsMixin,
-    SubjectRead,
-    ContributionReadWithoutEntityMixin,
+    # SubjectRead,
+    # BrainRegionReadMixin,
+    # AuthorizationMixin,
+    # IdentifiableMixin,
+    # CreationMixin,
+    # EntityTypeMixin,
+    # AssetsMixin,
+    # ContributionReadWithoutEntityMixin,
 ):
     id: uuid.UUID
 

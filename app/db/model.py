dc980b6eb641198160d2d9d0f5fa6f99a6d01db1
dkeller9 <daniel.keller@openbraininstitute.org>
2025-05-28 17:06:03 +0200

    more pylinting

--- app/db/model.py
+++ app/db/model.py
@@ -375,8 +375,10 @@ class Entity(LegacyMixin, Identifiable):
     }
 
 
-# Database model for PublicationBase
 class Publication(Entity, NameDescriptionVectorMixin):
+    """
+    Database model for PublicationBase
+    """
     __tablename__ = "publication"
     DOI: Mapped[Optional[str]] = mapped_column(String, nullable=True, index=True)
     PMID: Mapped[Optional[int]] = mapped_column(BigInteger, nullable=True, index=True)
@@ -389,13 +391,14 @@ class Publication(Entity, NameDescriptionVectorMixin):
     publication_year: Mapped[Optional[int]] = mapped_column(sa.Integer, nullable=True)
     abstract: Mapped[Optional[str]] = mapped_column(String, nullable=True)
 
- 
     __mapper_args__ = {
         "polymorphic_identity": __tablename__,
     }
 
-# Database model for PublishedInBase
 class PublishedIn(Base):
+    """
+    Database model for PublishedInBase
+    """
     __tablename__ = "published_in"
     publication_id: Mapped[UUID] = mapped_column(
         ForeignKey("publication.id"), primary_key=True, index=True
@@ -424,6 +427,7 @@ class PublishedIn(Base):
         {"extend_existing": True},
     )
 
+
 class ScientificArtifact(
     Entity,
     SubjectMixin,
@@ -431,15 +435,20 @@ class ScientificArtifact(
     LocationMixin,
     LicensedMixin
 ):
+    """
+    Base class for scientific artifacts,
+    which are entities that can be linked to publications.
+    """
     __tablename__ = "scientific_artifact"
     id: Mapped[uuid.UUID] = mapped_column(ForeignKey("entity.id"), primary_key=True)
     experiment_date: Mapped[datetime | None] = mapped_column(DateTime)
     published_in: Mapped[dict | None] = mapped_column(JSONB, nullable=True)
     contact_id: Mapped[uuid.UUID | None] = mapped_column(ForeignKey("person.id"), nullable=True)
- 
+
     __mapper_args__ = {
         "polymorphic_identity": __tablename__,
     }
+
 
 class SubjectMixin:
     subject_id: Mapped[uuid.UUID | None] = mapped_column(ForeignKey("subject.id"), index=True)
@@ -448,6 +457,7 @@ class SubjectMixin:
     @classmethod
     def subject(cls):
         return relationship("Subject", uselist=False, foreign_keys=cls.subject_id)
+
 
 class Subject(NameDescriptionVectorMixin, SpeciesMixin, Entity):
     __tablename__ = EntityType.subject.value

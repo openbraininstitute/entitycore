a59018f59117e7e2d6cf00a6a7513a22772def47
dkeller9 <daniel.keller@openbraininstitute.org>
2025-05-28 15:18:57 +0200

    Update scientific_artifact.py

--- app/schemas/scientific_artifact.py
+++ app/schemas/scientific_artifact.py
@@ -1,26 +1,25 @@
 import uuid
 from uuid import UUID
-from datetime import date, datetime, timedelta
-from typing import Annotated, TYPE_CHECKING, List, Optional
-from pydantic import UUID4, BaseModel, ConfigDict
+from datetime import date, datetime
+from typing import  Optional, List, TypedDict
+from pydantic import BaseModel, ConfigDict
 
-from app.schemas.agent import PersonRead
-from app.schemas.contribution import ContributionReadWithoutEntity
- 
-from app.schemas.contribution import ContributionRead
-from app.schemas.asset import AssetRead
-from app.schemas.base import LicenseRead,BrainRegionRead, EntityTypeMixin
-from app.db.model import Entity
-from enum import Enum, auto
+from app.schemas.base import BrainRegionRead
+from enum import Enum
+from app.schemas.entity import EntityRead
 
 #the nature of the publication
-class PubType(str, Enum):
+class PublicationType(str, Enum):
     EntitySource = "EntitySource" #where the model is published
-    ComponentSource  = "ComponentSource" #source of any one item used in the model)
+    ComponentSource  = "ComponentSource" #source of any one item used in the model
     Application  = "Application" #any publication which uses the model 
 
+class Author(TypedDict):
+    name: str
+    lastname: str
+
 # where was the artifact published?
-class PublishedIn(BaseModel):
+class PublicationBase(EntityRead):
     """
     Represents ways to define or identify an entity, such as by DOI, PMID, UUID, or other means.
     All fields are optional.
@@ -28,30 +27,40 @@ class PublishedIn(BaseModel):
     DOI: str | None = None  # Optional DOI (Digital Object Identifier) as a string
     PMID: int | None = None # Optional PMID (PubMed Identifier) as an integer
     UUID: uuid.UUID | None = None # Using uuid.UUID type for clarity, assuming it refers to a standard UUID
-    original_source_location : str
+    original_source_location : str| None = None 
     other: str | None = None # Optional alternative identifier as a string
-    publication_type: PubType | None = None # Optional publication type as a string
+    title: str | None = None 
+    authors: List[Author]| None = None #list of {"name", "lastname"} entries
+    url: str | None = None 
+    journal: str | None = None 
+    publication_year: int | None = None 
+    abstract: str | None = None 
 
-class ScientificArtifactBase(EntityTypeMixin):
+class PublicationCreate(PublicationBase):
+    pass
+
+class PublishedInBase(BaseModel):
+    publication_id: UUID
+    publication_type: PublicationType
+    scientific_artifact_id: UUID    
+
+class PublishedInCreate(PublishedInBase):
+    pass
+
+class ScientificArtifactBase(EntityRead):
     model_config = ConfigDict(from_attributes=True)
-    creation_date: datetime| None = None
-    update_date: Optional[datetime] = None
     name: str
-    description: Optional[str] = None
+    description: str
     subject_id : uuid.UUID | None = None
     license_id: Optional[UUID] = None
-    authorized_project_id: UUID
-    authorized_public: bool = False
-    createdBy_id: uuid.UUID 
-    updatedBy_id: Optional[UUID] 
     experiment_date: date | None = None 
-    published_in : list[PublishedIn]| None = None
     contact_id : uuid.UUID | None = None
     
-
 class ScientificArtifactCreate(ScientificArtifactBase):
     brain_region_id: uuid.UUID | None = None
 
 class ScientificArtifactRead(ScientificArtifactBase):
     id: UUID
     brain_region: BrainRegionRead
+    creation_date: datetime| None = None
+    update_date: datetime = None 

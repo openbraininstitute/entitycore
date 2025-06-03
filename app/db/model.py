9b24b6079cafc90396f733e09305d1a96ee1aa26
dkeller9 <daniel.keller@openbraininstitute.org>
2025-06-03 11:19:23 +0200

    added other_identifier

--- app/db/model.py
+++ app/db/model.py
@@ -386,7 +386,7 @@ class Publication(Entity, NameDescriptionVectorMixin):
     DOI: Mapped[str | None] = mapped_column(String, nullable=True, index=True)
     PMID: Mapped[int | None] = mapped_column(BigInteger, nullable=True, index=True)
     original_source_location: Mapped[str | None] = mapped_column(String, nullable=True)
-    other: Mapped[str | None] = mapped_column(String, nullable=True)
+    other_identifier: Mapped[str | None] = mapped_column(String, nullable=True)
     title: Mapped[str | None] = mapped_column(String, nullable=True)
     authors: Mapped[list[Author] | None] = mapped_column(JSONB, nullable=True)
     url: Mapped[str | None] = mapped_column(String, nullable=True)

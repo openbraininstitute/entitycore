260a7c504eacb6a4af05d3f0583d95bc5383ae18
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-06-13 20:15:21 +0200

    Use correct wit_prefix for used/generated nested filters

--- app/filters/activity.py
+++ app/filters/activity.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 from typing import Annotated
 
+from fastapi_filter import FilterDepends, with_prefix
+
 from app.filters.common import (
     CreationFilterMixin,
     CreatorFilterMixin,
     IdFilterMixin,
     NestedEntityFilter,
-    NestedEntityFilterDep,
 )
+
+NestedUsedFilterDep = FilterDepends(with_prefix("used", NestedEntityFilter))
+NestedGeneratedFilterDep = FilterDepends(with_prefix("generated", NestedEntityFilter))
 
 
 class ActivityFilterMixin(IdFilterMixin, CreatorFilterMixin, CreationFilterMixin):
     start_time: datetime | None = None
     end_time: datetime | None = None
 
-    used: Annotated[NestedEntityFilter | None, NestedEntityFilterDep] = None
-    generated: Annotated[NestedEntityFilter | None, NestedEntityFilterDep] = None
+    used: Annotated[NestedEntityFilter | None, NestedUsedFilterDep] = None
+    generated: Annotated[NestedEntityFilter | None, NestedGeneratedFilterDep] = None

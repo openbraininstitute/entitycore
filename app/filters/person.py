081504b047f0d6d14d37b1f87104911bb9bbfe95
bilalesi <b_meddah@esi.dz>
2025-06-21 21:35:42 +0200

    add filter by sub_id and other filters for person

---
+++ app/filters/person.py
@@ -0,0 +1,31 @@
+import uuid
+from typing import Annotated
+
+from fastapi_filter import FilterDepends
+
+from app.db.model import Person
+from app.filters.common import AgentFilter, CreatorFilterMixin
+
+
+class PersonFilter(AgentFilter, CreatorFilterMixin):
+    given_name: str | None = None
+    given_name__ilike: str | None = None
+    family_name: str | None = None
+    family_name__ilike: str | None = None
+    sub_id: uuid.UUID | None = None
+    sub_id__in: list[uuid.UUID] | None = None
+
+    order_by: list[str] = ["-creation_date"]  # noqa: RUF012
+
+    class Constants(AgentFilter.Constants):
+        model = Person
+        ordering_model_fields = [  # noqa: RUF012
+            "creation_date",
+            "update_date",
+            "pref_label",
+            "given_name",
+            "family_name",
+        ]
+
+
+PersonFilterDep = Annotated[PersonFilter, FilterDepends(PersonFilter)]

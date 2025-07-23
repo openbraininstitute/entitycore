50b67444805af65795f5a4b0a3643a66aaae0804
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-07-23 16:37:45 +0200

    Add filtering and tests

--- app/service/organization.py
+++ app/service/organization.py
@@ -1,15 +1,21 @@
 import uuid
+from typing import TYPE_CHECKING
 
 import sqlalchemy as sa
-from sqlalchemy.orm import joinedload, raiseload
+from sqlalchemy.orm import aliased, joinedload, raiseload
 
 import app.queries.common
-from app.db.model import Organization
+from app.db.model import Agent, Organization
 from app.dependencies.auth import AdminContextDep
 from app.dependencies.common import PaginationQuery
 from app.dependencies.db import SessionDep
+from app.filters.organization import OrganizationFilterDep
+from app.queries.factory import query_params_factory
 from app.schemas.agent import OrganizationCreate, OrganizationRead
-from app.schemas.types import ListResponse, PaginationResponse
+from app.schemas.types import ListResponse
+
+if TYPE_CHECKING:
+    from app.filters.base import Aliases
 
 
 def _load(query: sa.Select):
@@ -23,26 +29,42 @@ def _load(query: sa.Select):
 def read_many(
     db: SessionDep,
     pagination_request: PaginationQuery,
+    filter_model: OrganizationFilterDep,
 ) -> ListResponse[OrganizationRead]:
-    query = sa.select(Organization)
-
-    data = db.execute(
-        query.offset(pagination_request.offset).limit(pagination_request.page_size)
-    ).scalars()
-
-    total_items = db.execute(query.with_only_columns(sa.func.count(Organization.id))).scalar_one()
-
-    response = ListResponse[OrganizationRead](
-        data=[OrganizationRead.model_validate(d) for d in data],
-        pagination=PaginationResponse(
-            page=pagination_request.page,
-            page_size=pagination_request.page_size,
-            total_items=total_items,
-        ),
-        facets=None,
+    created_by_alias = aliased(Agent, flat=True)
+    updated_by_alias = aliased(Agent, flat=True)
+    aliases: Aliases = {
+        Agent: {
+            "created_by": created_by_alias,
+            "updated_by": updated_by_alias,
+        }
+    }
+    filter_keys = [
+        "created_by",
+        "updated_by",
+    ]
+    _, filter_joins = query_params_factory(
+        db_model_class=Organization,
+        filter_keys=filter_keys,
+        facet_keys=[],
+        aliases=aliases,
     )
-
-    return response
+    return app.queries.common.router_read_many(
+        db=db,
+        db_model_class=Organization,
+        authorized_project_id=None,
+        with_search=None,
+        with_in_brain_region=None,
+        facets=None,
+        aliases=aliases,
+        apply_filter_query_operations=None,
+        apply_data_query_operations=_load,
+        pagination_request=pagination_request,
+        response_schema_class=OrganizationRead,
+        name_to_facet_query_params=None,
+        filter_model=filter_model,
+        filter_joins=filter_joins,
+    )
 
 
 def read_one(id_: uuid.UUID, db: SessionDep) -> OrganizationRead:
--- tests/test_consortium.py
+++ tests/test_consortium.py
@@ -1,4 +1,10 @@
-from tests.utils import MISSING_ID, MISSING_ID_COMPACT
+from time import sleep
+
+import pytest
+
+from app.db.model import Consortium
+
+from tests.utils import MISSING_ID, MISSING_ID_COMPACT, add_db, assert_request
 
 ROUTE = "/consortium"
 
@@ -44,3 +50,35 @@ def test_missing(client):
 
     response = client.get(f"{ROUTE}/notanumber")
     assert response.status_code == 422
+
+
+@pytest.fixture
+def models(db, person_id):
+    res = []
+    for i in range(3):
+        row = add_db(
+            db,
+            Consortium(
+                pref_label=f"org-{i}",
+                alternative_name=f"alt-{i}",
+                created_by_id=person_id,
+                updated_by_id=person_id,
+            ),
+        )
+        res.append(row)
+        sleep(0.1)
+    return res
+
+
+def test_filtering_sorting(client, models):
+    def req(query):
+        return assert_request(client.get, url=ROUTE, params=query).json()["data"]
+
+    data = req({"order_by": "creation_date"})
+    assert len(data) == len(models)
+
+    data = req({"pref_label__in": ["org-1", "org-2"], "order_by": "-creation_date"})
+    assert [d["pref_label"] for d in data] == ["org-2", "org-1"]
+
+    data = req({"alternative_name": "alt-1", "order_by": "creation_date"})
+    assert [d["alternative_name"] for d in data] == ["alt-1"]

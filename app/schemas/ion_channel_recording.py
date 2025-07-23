87030e13cd8eefd9e2310ba3a6ff9ef03a41d0fe
Aurélien <72930209+AurelienJaquier@users.noreply.github.com>
2025-07-23 16:31:48 +0200

    lint fix

--- app/schemas/ion_channel_recording.py
+++ app/schemas/ion_channel_recording.py
@@ -3,11 +3,11 @@ from typing import Annotated
 from pydantic import Field
 
 from app.db.types import IonChannel
+from app.schemas.contribution import ContributionReadWithoutEntityMixin
 from app.schemas.electrical_cell_recording import (
     ElectricalCellRecordingBase,
     ElectricalRecordingStimulusRead,
 )
-from app.schemas.contribution import ContributionReadWithoutEntityMixin
 from app.schemas.scientific_artifact import (
     ScientificArtifactCreate,
     ScientificArtifactRead,

cf9c5469bb85757ac52c98d0cc164cb1da12706a
Gianluca Ficarelli <gianluca.ficarelli@gmail.com>
2025-08-12 19:43:09 +0200

    Revert PublicationFilter fix

--- app/filters/publication.py
+++ app/filters/publication.py
@@ -5,7 +5,7 @@ from fastapi_filter import with_prefix
 from app.db.model import Publication
 from app.dependencies.filter import FilterDepends
 from app.filters.base import CustomFilter
-from app.filters.common import CreationFilterMixin, CreatorFilterMixin, IdFilterMixin
+from app.filters.common import EntityFilterMixin, IdFilterMixin
 
 
 class NestedPublicationFilter(
@@ -24,8 +24,7 @@ class NestedPublicationFilter(
 
 
 class PublicationFilter(
-    CreatorFilterMixin,
-    CreationFilterMixin,
+    EntityFilterMixin,
     NestedPublicationFilter,
 ):
     order_by: list[str] = ["-creation_date"]  # noqa: RUF012

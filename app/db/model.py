afbc0a63a5d8cff40cc0facf068fe20cc21e7b02
Jan Krepl <18519371+jankrepl@users.noreply.github.com>
2025-08-25 13:22:59 +0200

    Write migration script

--- app/db/model.py
+++ app/db/model.py
@@ -176,13 +176,14 @@ class BrainRegion(Identifiable):
     hierarchy_id: Mapped[uuid.UUID] = mapped_column(
         ForeignKey("brain_region_hierarchy.id"), index=True
     )
-    embedding = mapped_column(Vector(1536))
+    embedding: Mapped[Vector] = mapped_column(Vector(1536), nullable=False)
 
 
 class Species(Identifiable):
     __tablename__ = "species"
     name: Mapped[str] = mapped_column(unique=True, index=True)
     taxonomy_id: Mapped[str] = mapped_column(unique=True, index=True)
+    embedding: Mapped[Vector] = mapped_column(Vector(1536), nullable=False)
 
 
 class Strain(Identifiable):
@@ -191,6 +192,7 @@ class Strain(Identifiable):
     taxonomy_id: Mapped[str] = mapped_column(unique=True, index=True)
     species_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("species.id"), index=True)
     species = relationship("Species", uselist=False)
+    embedding: Mapped[Vector] = mapped_column(Vector(1536), nullable=False)
 
     __table_args__ = (
         # needed for the composite foreign key in SpeciesMixin

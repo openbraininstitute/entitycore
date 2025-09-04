cbbebac4e63c33af7d555754bbf2c9676eedf2e4
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-09-04 14:06:16 +0200

    Add single_neuron_simulation update

--- app/routers/single_neuron_simulation.py
+++ app/routers/single_neuron_simulation.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 import app.service.single_neuron_simulation
 
 router = APIRouter(
-    prefix="/single-neuron-simulation",
-    tags=["single-neuron-simulation"],
+    prefix="/single_neuron_simulation",
+    tags=["single_neuron_simulation"],
 )
 
 read_many = router.get("")(app.service.single_neuron_simulation.read_many)
 read_one = router.get("/{id_}")(app.service.single_neuron_simulation.read_one)
 create_one = router.post("")(app.service.single_neuron_simulation.create_one)
+update_one = router.patch("/{id_}")(app.service.single_neuron_simulation.update_one)

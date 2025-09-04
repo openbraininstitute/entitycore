cd87da2b93b736c622f58f2b323068568a1f85ff
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-09-04 14:16:15 +0200

    Add single_neuron_synaptome

--- app/routers/single_neuron_synaptome.py
+++ app/routers/single_neuron_synaptome.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 import app.service.single_neuron_synaptome
 
 router = APIRouter(
-    prefix="/single-neuron-synaptome",
-    tags=["single-neuron-synaptome"],
+    prefix="/single_neuron_synaptome",
+    tags=["single_neuron_synaptome"],
 )
 
 read_many = router.get("")(app.service.single_neuron_synaptome.read_many)
 read_one = router.get("/{id_}")(app.service.single_neuron_synaptome.read_one)
 create_one = router.post("")(app.service.single_neuron_synaptome.create_one)
+update_one = router.patch("/{id_}")(app.service.single_neuron_synaptome.update_one)
--- app/service/single_neuron_synaptome.py
+++ app/service/single_neuron_synaptome.py
@@ -13,9 +13,18 @@ from app.dependencies.common import (
 )
 from app.dependencies.db import SessionDep
 from app.filters.single_neuron_synaptome import SingleNeuronSynaptomeFilterDep
-from app.queries.common import router_create_one, router_read_many, router_read_one
+from app.queries.common import (
+    router_create_one,
+    router_read_many,
+    router_read_one,
+    router_update_one,
+)
 from app.queries.factory import query_params_factory
-from app.schemas.synaptome import SingleNeuronSynaptomeCreate, SingleNeuronSynaptomeRead
+from app.schemas.synaptome import (
+    SingleNeuronSynaptomeCreate,
+    SingleNeuronSynaptomeRead,
+    SingleNeuronSynaptomeUpdate,
+)
 from app.schemas.types import ListResponse
 
 
@@ -58,6 +67,23 @@ def create_one(
         json_model=json_model,
         user_context=user_context,
         db_model_class=SingleNeuronSynaptome,
+        response_schema_class=SingleNeuronSynaptomeRead,
+        apply_operations=_load,
+    )
+
+
+def update_one(
+    user_context: UserContextDep,
+    db: SessionDep,
+    id_: uuid.UUID,
+    json_model: SingleNeuronSynaptomeUpdate,
+) -> SingleNeuronSynaptomeRead:
+    return router_update_one(
+        id_=id_,
+        db=db,
+        db_model_class=SingleNeuronSynaptome,
+        user_context=user_context,
+        json_model=json_model,
         response_schema_class=SingleNeuronSynaptomeRead,
         apply_operations=_load,
     )

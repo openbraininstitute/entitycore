242188494079f99e1b895f1b5d7091ef6976926a
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-09-04 14:00:36 +0200

    add subject update

--- app/routers/subject.py
+++ app/routers/subject.py
@@ -10,3 +10,4 @@ router = APIRouter(
 read_many = router.get("")(app.service.subject.read_many)
 read_one = router.get("/{id_}")(app.service.subject.read_one)
 create_one = router.post("")(app.service.subject.create_one)
+update_one = router.patch("/{id_}")(app.service.subject.update_one)

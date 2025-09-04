4571aacc303eec1a72dd5271fa7bc81610831a47
Juan Jose Garcia Cantero <juanjose.garcia@openbraininstitute.org>
2025-09-04 11:12:14 +0200

    Adapts license and role services to use router functions

--- app/service/license.py
+++ app/service/license.py
@@ -1,47 +1,44 @@
 import uuid
 
-import sqlalchemy as sa
-
 from app.db.model import License
 from app.dependencies.auth import AdminContextDep
 from app.dependencies.common import PaginationQuery
 from app.dependencies.db import SessionDep
-from app.errors import ensure_result
-from app.queries.common import router_create_one
+from app.filters.license import LicenseFilterDep
+from app.queries.common import router_create_one, router_read_many, router_read_one
 from app.schemas.base import LicenseCreate, LicenseRead
-from app.schemas.types import ListResponse, PaginationResponse
+from app.schemas.types import ListResponse
 
 
 def read_many(
-    db: SessionDep,
-    pagination_request: PaginationQuery,
+    db: SessionDep, pagination_request: PaginationQuery, filter_model: LicenseFilterDep
 ) -> ListResponse[LicenseRead]:
-    query = sa.select(License)
-
-    data = db.execute(
-        query.offset(pagination_request.offset).limit(pagination_request.page_size)
-    ).scalars()
-
-    total_items = db.execute(query.with_only_columns(sa.func.count(License.id))).scalar_one()
-
-    response = ListResponse[LicenseRead](
-        data=[LicenseRead.model_validate(d) for d in data],
-        pagination=PaginationResponse(
-            page=pagination_request.page,
-            page_size=pagination_request.page_size,
-            total_items=total_items,
-        ),
+    return router_read_many(
+        db=db,
+        db_model_class=License,
+        authorized_project_id=None,
+        with_search=None,
+        with_in_brain_region=None,
         facets=None,
+        aliases=None,
+        apply_filter_query_operations=None,
+        apply_data_query_operations=None,
+        pagination_request=pagination_request,
+        response_schema_class=LicenseRead,
+        name_to_facet_query_params=None,
+        filter_model=filter_model,
     )
-
-    return response
 
 
 def read_one(id_: uuid.UUID, db: SessionDep) -> LicenseRead:
-    with ensure_result(error_message="License not found"):
-        stmt = sa.select(License).filter(License.id == id_)
-        row = db.execute(stmt).scalar_one()
-    return LicenseRead.model_validate(row)
+    return router_read_one(
+        id_=id_,
+        db=db,
+        db_model_class=License,
+        authorized_project_id=None,
+        response_schema_class=LicenseRead,
+        apply_operations=None,
+    )
 
 
 def create_one(

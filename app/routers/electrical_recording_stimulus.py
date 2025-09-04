d3f68ad98d2ea517bef3a5414d3795aae924844e
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-09-04 11:36:26 +0200

    Add electrical_recording_stimulus update_one

--- app/routers/electrical_recording_stimulus.py
+++ app/routers/electrical_recording_stimulus.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 import app.service.electrical_recording_stimulus
 
 router = APIRouter(
-    prefix="/electrical-recording-stimulus",
-    tags=["electrical-recording-stimulus"],
+    prefix="/electrical_recording_stimulus",
+    tags=["electrical_recording_stimulus"],
 )
 
 read_many = router.get("")(app.service.electrical_recording_stimulus.read_many)
 read_one = router.get("/{id_}")(app.service.electrical_recording_stimulus.read_one)
 create_one = router.post("")(app.service.electrical_recording_stimulus.create_one)
+update_one = router.patch("/{id_}")(app.service.electrical_recording_stimulus.update_one)

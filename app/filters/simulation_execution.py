3ceb9361ee74738cf3772a1395ee0232c3fb30a9
Gianluca Ficarelli <26835404+GianlucaFicarelli@users.noreply.github.com>
2025-09-17 10:37:19 +0200

    Validate types in SimulationExecutionFilter and ElectricalRecordingStimulusFilter (#369)

--- app/filters/simulation_execution.py
+++ app/filters/simulation_execution.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from app.db.model import SimulationExecution
+from app.db.types import SimulationExecutionStatus
 from app.dependencies.filter import FilterDepends
 from app.filters.activity import ActivityFilterMixin
 from app.filters.base import CustomFilter
@@ -9,7 +10,7 @@ from app.filters.base import CustomFilter
 class SimulationExecutionFilter(CustomFilter, ActivityFilterMixin):
     order_by: list[str] = ["-creation_date"]  # noqa: RUF012
 
-    status: str | None = None
+    status: SimulationExecutionStatus | None = None
 
     class Constants(CustomFilter.Constants):
         model = SimulationExecution

0f29287e0c34376dd4184719aa4f3fd54cd02013
Aurélien <72930209+AurelienJaquier@users.noreply.github.com>
2025-09-30 13:59:19 +0200

    still trying to fix tests

--- app/schemas/ion_channel_model.py
+++ app/schemas/ion_channel_model.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from pydantic import BaseModel, ConfigDict, Field
 
 from app.schemas.asset import AssetsMixin
 from app.schemas.base import BrainRegionReadMixin, CreationMixin
+
+if TYPE_CHECKING:
+    from app.schemas.emodel import EModelRead
 from app.schemas.scientific_artifact import (
     NestedScientificArtifactRead,
     ScientificArtifactCreate,
@@ -74,8 +79,6 @@ class IonChannelModelExpanded(
     IonChannelModelBase,
     ScientificArtifactRead,
 ):
-    # ruff: noqa: PLC0415
-    # import from inside the class to avoid circular imports
-    from app.schemas.emodel import EModelRead
+    emodels: list["EModelRead"]   # use string type hint here to avoid cyclic import
 
-    emodels: list[EModelRead]
+IonChannelModelExpanded.model_rebuild()  # to avoid PydanticUserError

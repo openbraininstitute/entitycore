6747fd59a71233376564034a95018ba2163d5cbb
Aurélien <72930209+AurelienJaquier@users.noreply.github.com>
2025-10-13 15:01:53 +0200

    make cell_line optional in filters

--- app/filters/ion_channel_recording.py
+++ app/filters/ion_channel_recording.py
@@ -15,7 +15,7 @@ class IonChannelRecordingFilter(ScientificArtifactFilter, NameFilterMixin):
     recording_origin__in: list[ElectricalRecordingOrigin] | None = None
     temperature: float | None = None
     ion_channel: Annotated[NestedIonChannelFilter | None, NestedIonChannelFilterDep] = None
-    cell_line: str
+    cell_line: str | None = None
 
     order_by: list[str] = ["-creation_date"]  # noqa: RUF012
 
@@ -31,6 +31,8 @@ class IonChannelRecordingFilter(ScientificArtifactFilter, NameFilterMixin):
             "ion_channel__name",
             "ion_channel__label",
             "ion_channel__gene",
+            "temperature",
+            "cell_line",
         ]
 
 

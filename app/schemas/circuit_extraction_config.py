78f6bfb8e837601d95803f9678c2a442779e5055
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-10-17 17:34:19 +0200

    Added schemas

---
+++ app/schemas/circuit_extraction_config.py
@@ -0,0 +1,54 @@
+import uuid
+
+from pydantic import BaseModel, ConfigDict
+
+from app.db.types import JSON_DICT
+from app.schemas.agent import CreatedByUpdatedByMixin
+from app.schemas.asset import AssetsMixin
+from app.schemas.base import (
+    AuthorizationMixin,
+    AuthorizationOptionalPublicMixin,
+    CreationMixin,
+    EntityTypeMixin,
+    IdentifiableMixin,
+)
+from app.schemas.utils import make_update_schema
+
+
+class CircuitExtractionConfigBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: str
+    description: str
+    circuit_id: uuid.UUID
+    scan_parameters: JSON_DICT
+
+
+class CircuitExtractionConfigCreate(CircuitExtractionConfigBase, AuthorizationOptionalPublicMixin):
+    pass
+
+
+CircuitExtractionConfigUserUpdate = make_update_schema(
+    CircuitExtractionConfigCreate, "CircuitExtractionConfigUserUpdate"
+)  # pyright: ignore [reportInvalidTypeForm]
+
+CircuitExtractionConfigAdminUpdate = make_update_schema(
+    CircuitExtractionConfigCreate,
+    "CircuitExtractionConfigAdminUpdate",
+    excluded_fields=set(),
+)  # pyright : ignore [reportInvalidTypeForm]
+
+
+class NestedCircuitExtractionConfigRead(
+    CircuitExtractionConfigBase, EntityTypeMixin, IdentifiableMixin
+):
+    pass
+
+
+class CircuitExtractionConfigRead(
+    NestedCircuitExtractionConfigRead,
+    AssetsMixin,
+    CreatedByUpdatedByMixin,
+    CreationMixin,
+    AuthorizationMixin,
+):
+    pass

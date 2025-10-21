0e3427d1136b478854a5832d2222816d730bdf41
Christoph Pokorny <christoph.pokorny@openbraininstitute.org>
2025-10-21 09:31:59 +0200

    Added services

---
+++ app/service/circuit_extraction_campaign.py
@@ -0,0 +1,186 @@
+import uuid
+from typing import TYPE_CHECKING
+
+import sqlalchemy as sa
+from sqlalchemy.orm import aliased, joinedload, raiseload, selectinload
+
+from app.db.model import (
+    Agent,
+    CircuitExtractionCampaign,
+)
+from app.dependencies.auth import UserContextDep, UserContextWithProjectIdDep
+from app.dependencies.common import (
+    FacetsDep,
+    InBrainRegionDep,
+    PaginationQuery,
+    SearchDep,
+)
+from app.dependencies.db import SessionDep
+from app.filters.circuit_extraction_campaign import CircuitExtractionCampaignFilterDep
+from app.queries.common import (
+    router_create_one,
+    router_delete_one,
+    router_read_many,
+    router_read_one,
+    router_update_one,
+)
+from app.queries.factory import query_params_factory
+from app.schemas.circuit_extraction_campaign import (
+    CircuitExtractionCampaignAdminUpdate,
+    CircuitExtractionCampaignCreate,
+    CircuitExtractionCampaignRead,
+    CircuitExtractionCampaignUserUpdate,
+)
+from app.schemas.routers import DeleteResponse
+from app.schemas.types import ListResponse
+
+if TYPE_CHECKING:
+    from app.filters.base import Aliases
+
+
+def _load(query: sa.Select):
+    return query.options(
+        joinedload(CircuitExtractionCampaign.created_by),
+        joinedload(CircuitExtractionCampaign.updated_by),
+        selectinload(CircuitExtractionCampaign.assets),
+        selectinload(CircuitExtractionCampaign.contributions),
+        raiseload("*"),
+    )
+
+
+def read_one(
+    user_context: UserContextDep,
+    db: SessionDep,
+    id_: uuid.UUID,
+) -> CircuitExtractionCampaignRead:
+    return router_read_one(
+        db=db,
+        id_=id_,
+        db_model_class=CircuitExtractionCampaign,
+        authorized_project_id=user_context.project_id,
+        response_schema_class=CircuitExtractionCampaignRead,
+        apply_operations=_load,
+    )
+
+
+def admin_read_one(
+    db: SessionDep,
+    id_: uuid.UUID,
+) -> CircuitExtractionCampaignRead:
+    return router_read_one(
+        db=db,
+        id_=id_,
+        db_model_class=CircuitExtractionCampaign,
+        authorized_project_id=None,
+        response_schema_class=CircuitExtractionCampaignRead,
+        apply_operations=_load,
+    )
+
+
+def create_one(
+    db: SessionDep,
+    json_model: CircuitExtractionCampaignCreate,
+    user_context: UserContextWithProjectIdDep,
+) -> CircuitExtractionCampaignRead:
+    return router_create_one(
+        db=db,
+        json_model=json_model,
+        user_context=user_context,
+        db_model_class=CircuitExtractionCampaign,
+        response_schema_class=CircuitExtractionCampaignRead,
+        apply_operations=_load,
+    )
+
+
+def update_one(
+    user_context: UserContextDep,
+    db: SessionDep,
+    id_: uuid.UUID,
+    json_model: CircuitExtractionCampaignUserUpdate,  # pyright: ignore [reportInvalidTypeForm]
+) -> CircuitExtractionCampaignRead:
+    return router_update_one(
+        id_=id_,
+        db=db,
+        db_model_class=CircuitExtractionCampaign,
+        user_context=user_context,
+        json_model=json_model,
+        response_schema_class=CircuitExtractionCampaignRead,
+        apply_operations=_load,
+    )
+
+
+def admin_update_one(
+    db: SessionDep,
+    id_: uuid.UUID,
+    json_model: CircuitExtractionCampaignAdminUpdate,  # pyright: ignore [reportInvalidTypeForm]
+) -> CircuitExtractionCampaignRead:
+    return router_update_one(
+        id_=id_,
+        db=db,
+        db_model_class=CircuitExtractionCampaign,
+        user_context=None,
+        json_model=json_model,
+        response_schema_class=CircuitExtractionCampaignRead,
+        apply_operations=_load,
+    )
+
+
+def read_many(
+    user_context: UserContextDep,
+    db: SessionDep,
+    pagination_request: PaginationQuery,
+    filter_model: CircuitExtractionCampaignFilterDep,
+    with_search: SearchDep,
+    facets: FacetsDep,
+    in_brain_region: InBrainRegionDep,
+) -> ListResponse[CircuitExtractionCampaignRead]:
+    agent_alias = aliased(Agent, flat=True)
+    created_by_alias = aliased(Agent, flat=True)
+    updated_by_alias = aliased(Agent, flat=True)
+    aliases: Aliases = {
+        Agent: {
+            "contribution": agent_alias,
+            "created_by": created_by_alias,
+            "updated_by": updated_by_alias,
+        },
+    }
+    facet_keys = filter_keys = [
+        "created_by",
+        "updated_by",
+        "contribution",
+    ]
+    name_to_facet_query_params, filter_joins = query_params_factory(
+        db_model_class=CircuitExtractionCampaign,
+        facet_keys=facet_keys,
+        filter_keys=filter_keys,
+        aliases=aliases,
+    )
+    return router_read_many(
+        db=db,
+        filter_model=filter_model,
+        db_model_class=CircuitExtractionCampaign,
+        with_search=with_search,
+        with_in_brain_region=in_brain_region,
+        facets=facets,
+        name_to_facet_query_params=name_to_facet_query_params,
+        apply_filter_query_operations=None,
+        apply_data_query_operations=_load,
+        aliases=aliases,
+        pagination_request=pagination_request,
+        response_schema_class=CircuitExtractionCampaignRead,
+        authorized_project_id=user_context.project_id,
+        filter_joins=filter_joins,
+    )
+
+
+def delete_one(
+    user_context: UserContextDep,
+    db: SessionDep,
+    id_: uuid.UUID,
+) -> DeleteResponse:
+    return router_delete_one(
+        id_=id_,
+        db=db,
+        db_model_class=CircuitExtractionCampaign,
+        user_context=user_context,
+    )
---
+++ app/service/circuit_extraction_execution.py
@@ -0,0 +1,190 @@
+import uuid
+from typing import TYPE_CHECKING
+
+import sqlalchemy as sa
+from sqlalchemy.orm import aliased, joinedload, raiseload
+
+from app.db.model import Agent, CircuitExtractionExecution, Entity
+from app.dependencies.auth import UserContextDep, UserContextWithProjectIdDep
+from app.dependencies.common import (
+    FacetsDep,
+    InBrainRegionDep,
+    PaginationQuery,
+    SearchDep,
+)
+from app.dependencies.db import SessionDep
+from app.filters.circuit_extraction_execution import CircuitExtractionExecutionFilterDep
+from app.queries.common import (
+    router_create_activity_one,
+    router_delete_one,
+    router_read_many,
+    router_read_one,
+    router_update_activity_one,
+)
+from app.queries.factory import query_params_factory
+from app.schemas.circuit_extraction_execution import (
+    CircuitExtractionExecutionAdminUpdate,
+    CircuitExtractionExecutionCreate,
+    CircuitExtractionExecutionRead,
+    CircuitExtractionExecutionUserUpdate,
+)
+from app.schemas.routers import DeleteResponse
+from app.schemas.types import ListResponse
+
+if TYPE_CHECKING:
+    from app.filters.base import Aliases
+
+
+def _load(query: sa.Select):
+    return query.options(
+        joinedload(CircuitExtractionExecution.used),
+        joinedload(CircuitExtractionExecution.generated),
+        joinedload(CircuitExtractionExecution.created_by),
+        joinedload(CircuitExtractionExecution.updated_by),
+        raiseload("*"),
+    )
+
+
+def read_one(
+    user_context: UserContextDep,
+    db: SessionDep,
+    id_: uuid.UUID,
+) -> CircuitExtractionExecutionRead:
+    return router_read_one(
+        db=db,
+        id_=id_,
+        db_model_class=CircuitExtractionExecution,
+        authorized_project_id=user_context.project_id,
+        response_schema_class=CircuitExtractionExecutionRead,
+        apply_operations=_load,
+    )
+
+
+def admin_read_one(
+    db: SessionDep,
+    id_: uuid.UUID,
+) -> CircuitExtractionExecutionRead:
+    return router_read_one(
+        db=db,
+        id_=id_,
+        db_model_class=CircuitExtractionExecution,
+        authorized_project_id=None,
+        response_schema_class=CircuitExtractionExecutionRead,
+        apply_operations=_load,
+    )
+
+
+def create_one(
+    db: SessionDep,
+    json_model: CircuitExtractionExecutionCreate,
+    user_context: UserContextWithProjectIdDep,
+) -> CircuitExtractionExecutionRead:
+    return router_create_activity_one(
+        db=db,
+        json_model=json_model,
+        user_context=user_context,
+        db_model_class=CircuitExtractionExecution,
+        response_schema_class=CircuitExtractionExecutionRead,
+        apply_operations=_load,
+    )
+
+
+def read_many(
+    user_context: UserContextDep,
+    db: SessionDep,
+    pagination_request: PaginationQuery,
+    filter_model: CircuitExtractionExecutionFilterDep,
+    with_search: SearchDep,
+    facets: FacetsDep,
+    in_brain_region: InBrainRegionDep,
+) -> ListResponse[CircuitExtractionExecutionRead]:
+    created_by_alias = aliased(Agent, flat=True)
+    updated_by_alias = aliased(Agent, flat=True)
+    used_alias = aliased(Entity, flat=True)
+    generated_alias = aliased(Entity, flat=True)
+
+    aliases: Aliases = {
+        Agent: {
+            "created_by": created_by_alias,
+            "updated_by": updated_by_alias,
+        },
+        Entity: {
+            "used": used_alias,
+            "generated": generated_alias,
+        },
+    }
+    facet_keys = []
+    filter_keys = [
+        "created_by",
+        "updated_by",
+        "used",
+        "generated",
+    ]
+    name_to_facet_query_params, filter_joins = query_params_factory(
+        db_model_class=CircuitExtractionExecution,
+        facet_keys=facet_keys,
+        filter_keys=filter_keys,
+        aliases=aliases,
+    )
+    return router_read_many(
+        db=db,
+        filter_model=filter_model,
+        db_model_class=CircuitExtractionExecution,
+        with_search=with_search,
+        with_in_brain_region=in_brain_region,
+        facets=facets,
+        name_to_facet_query_params=name_to_facet_query_params,
+        apply_filter_query_operations=None,
+        apply_data_query_operations=_load,
+        aliases=aliases,
+        pagination_request=pagination_request,
+        response_schema_class=CircuitExtractionExecutionRead,
+        authorized_project_id=user_context.project_id,
+        filter_joins=filter_joins,
+    )
+
+
+def delete_one(
+    user_context: UserContextDep,
+    db: SessionDep,
+    id_: uuid.UUID,
+) -> DeleteResponse:
+    return router_delete_one(
+        id_=id_,
+        db=db,
+        db_model_class=CircuitExtractionExecution,
+        user_context=user_context,
+    )
+
+
+def update_one(
+    db: SessionDep,
+    id_: uuid.UUID,
+    json_model: CircuitExtractionExecutionUserUpdate,  # pyright: ignore [reportInvalidTypeForm]
+    user_context: UserContextWithProjectIdDep,
+) -> CircuitExtractionExecutionRead:
+    return router_update_activity_one(
+        db=db,
+        id_=id_,
+        json_model=json_model,
+        user_context=user_context,
+        db_model_class=CircuitExtractionExecution,
+        response_schema_class=CircuitExtractionExecutionRead,
+        apply_operations=_load,
+    )
+
+
+def admin_update_one(
+    db: SessionDep,
+    id_: uuid.UUID,
+    json_model: CircuitExtractionExecutionAdminUpdate,  # pyright: ignore [reportInvalidTypeForm]
+) -> CircuitExtractionExecutionRead:
+    return router_update_activity_one(
+        db=db,
+        id_=id_,
+        json_model=json_model,
+        user_context=None,
+        db_model_class=CircuitExtractionExecution,
+        response_schema_class=CircuitExtractionExecutionRead,
+        apply_operations=_load,
+    )

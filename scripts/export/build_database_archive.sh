7d3acb2053f3ea65f6c39b40e6dd22696cea8fe1
MikeG <mgevaert@openbraininstitute.org>
2025-11-07 14:22:24 +0100

    add SimulationExecutionStatus::cancelled (#437)

--- scripts/export/build_database_archive.sh
+++ scripts/export/build_database_archive.sh
@@ -2,7 +2,7 @@
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="83185c2aca1c"
+SCRIPT_DB_VERSION="40900849179f"
 echo "DB dump (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 
@@ -261,7 +261,7 @@ install -m 755 /dev/stdin "$WORK_DIR/load.sh" <<'EOF_LOAD_SCRIPT'
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="83185c2aca1c"
+SCRIPT_DB_VERSION="40900849179f"
 echo "DB load (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 

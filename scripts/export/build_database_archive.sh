ae8256e4d880c340d2c22056be13ff010561c4f1
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-11-19 11:28:57 +0100

    migration

--- scripts/export/build_database_archive.sh
+++ scripts/export/build_database_archive.sh
@@ -2,7 +2,7 @@
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="776de4a495dc"
+SCRIPT_DB_VERSION="83d37eb68507"
 echo "DB dump (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 
@@ -261,7 +261,7 @@ install -m 755 /dev/stdin "$WORK_DIR/load.sh" <<'EOF_LOAD_SCRIPT'
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="776de4a495dc"
+SCRIPT_DB_VERSION="83d37eb68507"
 echo "DB load (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 

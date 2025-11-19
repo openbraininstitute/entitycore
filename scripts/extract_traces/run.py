697cac88f221ab85d08dc5410fbddb17dbf71a8b
Gianluca Ficarelli <gianluca.ficarelli@openbraininstitute.org>
2025-11-19 15:15:49 +0100

    Extract read_one and read_many

--- scripts/extract_traces/run.py
+++ scripts/extract_traces/run.py
@@ -1,9 +1,13 @@
-import hashlib
+import hashlib  # noqa: INP001
 import json
+import logging
 import re
 from pathlib import Path
+from urllib import parse
 
 import click
+
+L = logging.getLogger("extract_traces")
 
 UUID_RE = re.compile(
     r"\b[0-9a-fA-F]{8}-"
@@ -27,9 +31,11 @@ def normalize(obj):  # noqa: PLR0911
             return "__UUID__"
         if TIMESTAMP_RE.fullmatch(obj):
             return "__TIMESTAMP__"
-        return obj
+        return "__STR__"
     if isinstance(obj, bool):
         return "__BOOL__"
+    if isinstance(obj, (int, float)):
+        return "__NUMBER__"
     return obj
 
 
@@ -63,32 +69,49 @@ def compute_checksum(content, length: int) -> str:
 )
 def extract(source: Path, component: str | None, output: Path) -> None:
     comp_regex = re.compile(component) if component else None
-    extract_segment = re.compile(r"^http://testserver/([^/]+)")
     for path in source.glob("*.json"):
         try:
             data = json.loads(path.read_text())
             request = data["request"]
             response = data["response"]
 
-            if request["method"] != "POST" or response["status_code"] not in {200, 201}:
+            if request["method"] not in {"GET", "POST"}:
+                # ignore PATCH, DELETE
                 continue
-            if not (match := extract_segment.match(request["url"])):
+            if response["status_code"] not in {200, 201}:
+                # ignore unsuccessful responses
                 continue
-            segment = match.group(1)
-            if comp_regex and not comp_regex.search(segment):
+
+            parsed_url = parse.urlparse(request["url"])
+            head, _, tail = parsed_url.path.strip("/").partition("/")
+            if head == "admin":
+                # ignore admin endpoints
+                continue
+            if comp_regex and not comp_regex.search(head):
+                # ignore not matching requests if component has been specified
+                continue
+            if request["method"] == "POST":
+                request_type = "create_one"
+            elif not tail:
+                request_type = "read_many"
+            elif UUID_RE.fullmatch(tail):
+                request_type = "read_one"
+            else:
+                L.warning(f"Ignored {request['method']} {parsed_url.path}, skipping {path}")
                 continue
 
             content = json.dumps(response["content_json"], sort_keys=True, indent=2)
             checksum = compute_checksum(response["content_json"], length=6)
 
-            out_path = output / segment / f"content_{checksum}.json"
+            out_path = output / request_type / head / f"content_{checksum}.json"
             out_path.parent.mkdir(parents=True, exist_ok=True)
             out_path.write_text(content)
 
         except (json.JSONDecodeError, KeyError, TypeError):
-            print(f"Malformed json or missing field, skipping {path}")  # noqa: T201
+            L.warning(f"Malformed json or missing field, skipping {path}")
             continue
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     extract()

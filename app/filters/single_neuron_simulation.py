7f331dc3df6c1cb6d49de4365df33efc21d16df5
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-12-02 22:36:10 +0100

    Add ILikeSearchFilterMixin to SingleNeuronSimulation

--- app/filters/single_neuron_simulation.py
+++ app/filters/single_neuron_simulation.py
@@ -6,6 +6,7 @@ from app.dependencies.filter import FilterDepends
 from app.filters.base import CustomFilter
 from app.filters.brain_region import BrainRegionFilterMixin
 from app.filters.common import (
+    ILikeSearchFilterMixin,
     NameFilterMixin,
 )
 from app.filters.entity import EntityFilterMixin
@@ -17,6 +18,7 @@ class SingleNeuronSimulationFilter(
     EntityFilterMixin,
     BrainRegionFilterMixin,
     NameFilterMixin,
+    ILikeSearchFilterMixin,
 ):
     status: SingleNeuronSimulationStatus | None = None
 

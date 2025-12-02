76968756a514d2fbb2e4824c0eee5d805d703956
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-12-02 22:55:50 +0100

    Add ILikeSearchFilterMixin to experimental density models

--- tests/test_experimental_synapses_per_connection.py
+++ tests/test_experimental_synapses_per_connection.py
@@ -369,3 +369,9 @@ def test_sorting_and_filtering(client, models):
 
         data = req({"created_by__sub_id": USER_SUB_ID_1, "updated_by__sub_id": USER_SUB_ID_1})
         assert len(data) == n_models
+
+        data = req({"ilike_search": "d"})
+        assert len(data) == n_models
+
+        data = req({"ilike_search": "s10"})
+        assert len(data) == 1

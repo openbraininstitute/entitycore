e3928cc76495312a0791cba4c5d12a9d51d16295
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-12-16 15:45:24 +0100

    Refactor NestedAgentFilter

--- app/filters/contribution.py
+++ app/filters/contribution.py
@@ -2,26 +2,20 @@ from typing import Annotated
 
 from fastapi_filter import with_prefix
 
-from app.db.model import Agent, Contribution
+from app.db.model import Contribution
 from app.dependencies.filter import FilterDepends
 from app.filters.base import CustomFilter
 from app.filters.common import (
     CreationFilterMixin,
     IdFilterMixin,
-    PrefLabelMixin,
+    NestedAgentFilter,
+    NestedAgentFilterDep,
 )
 from app.filters.entity import NestedEntityFilter, NestedEntityFilterDep
 from app.filters.person import CreatorFilterMixin
 
-
-class NestedAgentFilter(IdFilterMixin, PrefLabelMixin, CustomFilter):
-    class Constants(CustomFilter.Constants):
-        model = Agent
-
-
 NestedContributionFilter = NestedAgentFilter
 NestedContributionFilterDep = FilterDepends(with_prefix("contribution", NestedAgentFilter))
-NestedAgentFilterDep = FilterDepends(with_prefix("agent", NestedContributionFilter))
 
 
 class ContributionFilter(IdFilterMixin, CreationFilterMixin, CreatorFilterMixin, CustomFilter):

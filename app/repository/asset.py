56a9918ccaafeae6738195769168e58943d2395c
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-12-17 15:15:01 +0100

    Asset create ORM

--- app/repository/asset.py
+++ app/repository/asset.py
@@ -40,26 +40,24 @@ class AssetRepository(BaseRepository):
     def create_entity_asset(self, entity_id: uuid.UUID, asset: AssetCreate) -> Asset:
         """Create an asset associated with the given entity."""
         sha256_digest = bytes.fromhex(asset.sha256_digest) if asset.sha256_digest else None
-        query = (
-            sa.insert(Asset)
-            .values(
-                status=AssetStatus.CREATED,
-                entity_id=entity_id,
-                path=asset.path,
-                full_path=asset.full_path,
-                is_directory=asset.is_directory,
-                content_type=asset.content_type,
-                size=asset.size,
-                sha256_digest=sha256_digest,
-                meta=asset.meta,
-                label=asset.label,
-                storage_type=asset.storage_type,
-                created_by_id=asset.created_by_id,
-                updated_by_id=asset.updated_by_id,
-            )
-            .returning(Asset)
+        db_asset = Asset(
+            status=AssetStatus.CREATED,
+            entity_id=entity_id,
+            path=asset.path,
+            full_path=asset.full_path,
+            is_directory=asset.is_directory,
+            content_type=asset.content_type,
+            size=asset.size,
+            sha256_digest=sha256_digest,
+            meta=asset.meta,
+            label=asset.label,
+            storage_type=asset.storage_type,
+            created_by_id=asset.created_by_id,
+            updated_by_id=asset.updated_by_id,
         )
-        return self.db.execute(query).scalar_one()
+        self.db.add(db_asset)
+        self.db.flush()
+        return db_asset
 
     def delete_entity_asset(
         self,
@@ -76,4 +74,5 @@ class AssetRepository(BaseRepository):
         )
         asset = self.db.execute(query).scalar_one()
         self.db.delete(asset)
+        self.db.flush()
         return asset

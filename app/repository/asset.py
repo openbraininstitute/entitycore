ab667d087206bfcc08b511e8e5097404e59d2910
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-12-17 14:23:25 +0000

    Switch to asset ORM create/delete (#483)

--- app/repository/asset.py
+++ app/repository/asset.py
@@ -40,54 +40,24 @@ class AssetRepository(BaseRepository):
     def create_entity_asset(self, entity_id: uuid.UUID, asset: AssetCreate) -> Asset:
         """Create an asset associated with the given entity."""
         sha256_digest = bytes.fromhex(asset.sha256_digest) if asset.sha256_digest else None
-        query = (
-            sa.insert(Asset)
-            .values(
-                status=AssetStatus.CREATED,
-                entity_id=entity_id,
-                path=asset.path,
-                full_path=asset.full_path,
-                is_directory=asset.is_directory,
-                content_type=asset.content_type,
-                size=asset.size,
-                sha256_digest=sha256_digest,
-                meta=asset.meta,
-                label=asset.label,
-                storage_type=asset.storage_type,
-                created_by_id=asset.created_by_id,
-                updated_by_id=asset.updated_by_id,
-            )
-            .returning(Asset)
+        db_asset = Asset(
+            status=AssetStatus.CREATED,
+            entity_id=entity_id,
+            path=asset.path,
+            full_path=asset.full_path,
+            is_directory=asset.is_directory,
+            content_type=asset.content_type,
+            size=asset.size,
+            sha256_digest=sha256_digest,
+            meta=asset.meta,
+            label=asset.label,
+            storage_type=asset.storage_type,
+            created_by_id=asset.created_by_id,
+            updated_by_id=asset.updated_by_id,
         )
-        return self.db.execute(query).scalar_one()
-
-    def update_entity_asset_status(
-        self,
-        entity_type: EntityType,
-        entity_id: uuid.UUID,
-        asset_id: uuid.UUID,
-        asset_status: AssetStatus,
-    ) -> Asset:
-        """Update the status of the given asset.
-
-        Raise an error if any of the following is true:
-
-        - the asset doesn't exist for the given entity
-        - the status is already set with the same requested value
-        """
-        query = (
-            sa.update(Asset)
-            .values(status=asset_status)
-            .where(
-                Asset.entity_id == entity_id,
-                Asset.id == asset_id,
-                Asset.status != asset_status,
-                Entity.type == entity_type.name,
-                Entity.id == Asset.entity_id,
-            )
-            .returning(Asset)
-        )
-        return self.db.execute(query).scalar_one()
+        self.db.add(db_asset)
+        self.db.flush()
+        return db_asset
 
     def delete_entity_asset(
         self,
@@ -96,14 +66,13 @@ class AssetRepository(BaseRepository):
         asset_id: uuid.UUID,
     ) -> Asset:
         """Delete an entity asset."""
-        query = (
-            sa.delete(Asset)
-            .where(
-                Asset.entity_id == entity_id,
-                Asset.id == asset_id,
-                Entity.type == entity_type.name,
-                Entity.id == Asset.entity_id,
-            )
-            .returning(Asset)
+        query = sa.select(Asset).where(
+            Asset.entity_id == entity_id,
+            Asset.id == asset_id,
+            Entity.type == entity_type.name,
+            Entity.id == Asset.entity_id,
         )
-        return self.db.execute(query).scalar_one()
+        asset = self.db.execute(query).scalar_one()
+        self.db.delete(asset)
+        self.db.flush()
+        return asset

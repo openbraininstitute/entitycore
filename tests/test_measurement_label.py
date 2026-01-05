1af27984f20ead846a31d3bec6148b57a92bbbc9
Gianluca Ficarelli <gianluca.ficarelli@openbraininstitute.org>
2026-01-05 15:35:35 +0100

    Add entity_type filter to measurement_label

--- tests/test_measurement_label.py
+++ tests/test_measurement_label.py
@@ -140,3 +140,9 @@ def test_filtering(client, models):
 
     data = req({"created_by__sub_id": USER_SUB_ID_1, "updated_by__sub_id": USER_SUB_ID_1})
     assert len(data) == len(models)
+
+    data = req({"entity_type": EntityType.cell_morphology, "pref_label__ilike": "label_1"})
+    assert len(data) == 1
+
+    data = req({"entity_type": EntityType.em_cell_mesh})
+    assert len(data) == 0

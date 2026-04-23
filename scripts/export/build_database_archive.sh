70b8c2610de0cf16fd513995e1f107c9b2c0ff96
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-04-23 15:55:16 +0200

    brian2 -> Brian2 for real this time

--- scripts/export/build_database_archive.sh
+++ scripts/export/build_database_archive.sh
@@ -2,7 +2,7 @@
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="cdab088f422f"
+SCRIPT_DB_VERSION="30f66941ec3b"
 echo "DB dump (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 
@@ -269,7 +269,7 @@ install -m 755 /dev/stdin "$WORK_DIR/load.sh" <<'EOF_LOAD_SCRIPT'
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="cdab088f422f"
+SCRIPT_DB_VERSION="30f66941ec3b"
 echo "DB load (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 

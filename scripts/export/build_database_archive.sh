23c922c485b594e036150e646601d74e36948cf6
Daniel Keller <dkeller@mac.home>
2026-06-09 13:07:03 +0200

    resolve conflict

--- scripts/export/build_database_archive.sh
+++ scripts/export/build_database_archive.sh
@@ -2,7 +2,7 @@
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="96b2b5af4950"
+SCRIPT_DB_VERSION="267ed9485ccd"
 echo "DB dump (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 
@@ -271,7 +271,7 @@ install -m 755 /dev/stdin "$WORK_DIR/load.sh" <<'EOF_LOAD_SCRIPT'
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="96b2b5af4950"
+SCRIPT_DB_VERSION="267ed9485ccd"
 echo "DB load (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 

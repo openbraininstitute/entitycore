75f606f55e8fd7dd95423c9a142160649ad8ffa8
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2026-07-06 14:55:50 +0200

    Remove circuit extraction legacy code (#645)

--- app/routers/circuit_extraction_execution.py
+++
@@ -1,8 +0,0 @@
-import app.service.circuit_extraction_execution as service
-from app.routers.admin import router as admin_router
-from app.routers.common import create_user_router, register_default_admin_routes
-from app.routers.types import ActivityRoute
-
-ROUTE = ActivityRoute.circuit_extraction_execution
-router = create_user_router(route=ROUTE, service=service)
-register_default_admin_routes(router=admin_router, service=service, route=ROUTE)

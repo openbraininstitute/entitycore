84ff3aa0f076f06dab6bd16eee3b58ca7a450710
Gianluca Ficarelli <gianluca.ficarelli@openbraininstitute.org>
2026-08-10 15:37:55 +0200

    Add lfp_report asset label

--- scripts/export/build_database_archive.sh
+++ scripts/export/build_database_archive.sh
@@ -2,7 +2,7 @@
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="122ee418a384"
+SCRIPT_DB_VERSION="1695b8e6508c"
 echo "DB dump (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 
@@ -269,7 +269,7 @@ install -m 755 /dev/stdin "$WORK_DIR/load.sh" <<'EOF_LOAD_SCRIPT'
 # Automatically generated, do not edit!
 set -euo pipefail
 SCRIPT_VERSION="1"
-SCRIPT_DB_VERSION="122ee418a384"
+SCRIPT_DB_VERSION="1695b8e6508c"
 echo "DB load (version $SCRIPT_VERSION for db version $SCRIPT_DB_VERSION)"
 
 

987b7e7ee2c81e0fa47ccc7e8a80ab0e88684bb1
Gianluca Ficarelli <gianluca.ficarelli@openbraininstitute.org>
2026-08-17 14:23:14 +0200

    Update migration after merge

--- alembic/versions/20260817_000000_8953d8ad7437_add_uuid_ossp.py
+++ alembic/versions/20260817_000000_8953d8ad7437_add_uuid_ossp.py
@@ -1,8 +1,8 @@
 """add uuid-ossp
 
 Revision ID: 8953d8ad7437
-Revises: 1695b8e6508c
-Create Date: 2026-08-14 00:00:00.000000
+Revises: 5b0ea9253ea5
+Create Date: 2026-08-17 00:00:00.000000
 
 """
 
@@ -13,7 +13,7 @@ from alembic_utils.pg_extension import PGExtension
 
 # revision identifiers, used by Alembic.
 revision: str = "8953d8ad7437"
-down_revision: Union[str, None] = "1695b8e6508c"
+down_revision: Union[str, None] = "5b0ea9253ea5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
